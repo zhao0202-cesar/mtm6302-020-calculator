a74796ba1f3612b7d5780926ee3151d5e4e35f1a
Chang Zhao <zhao0202@algonquinlive.com>
2020-09-30 19:49:37 -0400

    good

---
+++ script.js
@@ -0,0 +1,23 @@
+//addition function
+function add(a, b) {
+    return a + b
+}
+
+//subtraction function
+function subtract(a, b) {
+    return a - b
+}
+
+//multiplication function
+function multiply(a, b){
+    return a * b
+}
+
+//division function
+function divide(a, b){
+    return a / b
+}
+
+//big long calculation
+console.log(add(subtract(1, 2), multiply(3, 4)))
+//           add(       -1     ,       12     )
